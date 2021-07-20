86c51e9966f864357d073d9fd889fc797295d88b
illbebach <illbebach>
2021-07-19 23:32:34 -0500

    cleanup relative import paths

--- rebalance/asset.py
+++ rebalance/asset.py
@@ -1,5 +1,5 @@
-from rebalance import Price
 
+from .price import Price
 import yfinance as yf
 
 
@@ -41,14 +41,14 @@ class Asset:
 
     @property
     def price(self):
-        """ 
-        (float): Price of the asset (in asset's own currency). 
+        """
+        (float): Price of the asset (in asset's own currency).
         """
         return self._price.price
 
     def price_in(self, currency):
-        """ 
-        Price of the asset in specified currency. 
+        """
+        Price of the asset in specified currency.
 
         Args:
             currency (str): Currency in which to obtain price of asset.
@@ -57,8 +57,8 @@ class Asset:
 
     @property
     def currency(self):
-        """ 
-        (str): Currency of the asset. 
+        """
+        (str): Currency of the asset.
         """
         return self._price.currency
 
@@ -71,7 +71,7 @@ class Asset:
 
     def market_value(self):
         """
-        Computes the market value of the asset. 
+        Computes the market value of the asset.
 
         Returns:
             float: Market value of the asset (in asset's own currency).
@@ -80,7 +80,7 @@ class Asset:
 
     def market_value_in(self, currency):
         """
-        Computes the market value of the asset in specified currency. 
+        Computes the market value of the asset in specified currency.
 
         Args:
             currency (str): Currency in which to obtain market value.
@@ -104,7 +104,7 @@ class Asset:
         self._quantity += quantity
         if currency is None:
             return self._price.price * quantity
-        
+
         return self._price.price_in(currency) * quantity
 
     def cost_of(self, units, currency=None):
@@ -120,7 +120,7 @@ class Asset:
         """
         if currency is None:
             return self.price * units
-      
+
         return self.price_in(currency) * units
 
     @property
@@ -128,5 +128,6 @@ class Asset:
         return 0.
 
     def __str__(self):
-        return yf.Ticker(
-            self._ticker).info['shortName'] + "(" + self._ticker + ")"
+        # return yf.Ticker(
+        #     self._ticker).info['shortName'] + " (" + self._ticker + ")"
+        return f"{self._ticker:4s}  {self._quantity:4d} {self.market_value():8.2f}"
